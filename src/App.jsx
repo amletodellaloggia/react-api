5b739acd91059c6abf2b8f85ef69ba68dd6f841d
amletodellaloggia <amletodellaloggia@gmail.com>
2025-09-02 14:19:28 +0200

    [upd] added static card to fill with actresses

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,30 @@ import axios from "axios";
 function App() {
   const [actresses, setActresses] = useState([]);
 
-  console.log({actresses});
   function fetchActresses() {
-    axios.get("https://lanciweb.github.io/demo/api/actresses/").then((res) => setActresses(res.data))
+    axios
+      .get("https://lanciweb.github.io/demo/api/actresses/")
+      .then((res) => setActresses(res.data));
   }
 
   return (
     <>
-
+    <h1>Lista Attrici</h1>
+    <div className="card" style={{ width: "18rem" }}>
+      <img src="..." className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">Nome</h5>
+        <p className="card-text">Biografia</p>
+      </div>
+      <ul className="list-group list-group-flush">
+        <li className="list-group-item">Anno Nascita</li>
+        <li className="list-group-item">Nazionalità</li>
+        <li className="list-group-item">Riconoscimenti</li>
+      </ul>
+    </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
