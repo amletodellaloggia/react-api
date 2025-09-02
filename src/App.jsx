a57030b45e471debc187c4b40f3286604b2ba32d
amletodellaloggia <amletodellaloggia@gmail.com>
2025-09-02 13:55:13 +0200

    [upd] imported useState, imported axios, added state var for actresses api, defined function to fetch actresses api, console.log test

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
+import axios from "axios";
+
 function App() {
+  const [actresses, setActresses] = useState([]);
+
+  console.log({actresses});
+  function fetchActresses() {
+    axios.get("https://lanciweb.github.io/demo/api/actresses/").then((res) => setActresses(res.data))
+  }
 
   return (
     <>
-    
+
     </>
   )
 }
